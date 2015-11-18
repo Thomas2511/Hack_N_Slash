94d435537c27d4012b23c9f02b35e070210cb028
Aymeric FAUCHER <afaucher@e3r8p7.42.fr>
2015-11-18 20:44:52 +0100

    boss

---
+++ rush01/Assets/BossFootScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossFootScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Ground")
+		{
+			Camera.main.GetComponent<CameraScript>().Shake ();
+			if (GetComponent<AudioSource>())
+				GetComponent<AudioSource>().Play();
+		}
+	}
+}
