d3073b4fa9464fa1cdde0da2ec3d99251196365c
tdieumeg <tdieumeg@student.42.fr>
2015-12-09 16:12:26 +0100

    Main menu

---
+++ rush01/Assets/Scripts/GUI/Main Menu/ButtonHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonHandler : MonoBehaviour {
+
+	// Use this for initialization
+	public void Newgame ()
+	{
+		Application.LoadLevel ("Dungeon2");
+	}
+
+	public void Loadgame ()
+	{
+	}
+
+	public void Exitgame ()
+	{
+		Application.Quit ();
+	}
+}
